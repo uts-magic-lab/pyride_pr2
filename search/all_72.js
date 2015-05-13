fc13ab97dd71c9c3dc55657e45bd7f5ac08dd847
Xun Wang <wang.xun@gmail.com>
2015-05-13 14:53:33 +1000

    doc update to include uts-specific functions.

--- search/all_72.js
+++ search/all_72.js
@@ -1,5 +1,8 @@
 var searchData=
 [
-  ['registerbasescancallback',['registerBaseScanCallback',['../classPyPR2.html#a6039673392cc2570503dc7ced173b4a6',1,'PyPR2']]],
-  ['registertiltscancallback',['registerTiltScanCallback',['../classPyPR2.html#ad36b823027470d77f7535b2944e63ec8',1,'PyPR2']]]
+  ['recallrhythdmptrajectory',['recallRhythDMPTrajectory',['../classPyPR2.html#a3f0c7512f5dc678767ac973010644abb',1,'PyPR2']]],
+  ['registerbasescancallback',['registerBaseScanCallback',['../classPyPR2.html#a73c49857323bd14f75c3301eca159b6e',1,'PyPR2']]],
+  ['registerhumandetecttracking',['registerHumanDetectTracking',['../classPyPR2.html#a3f510a5817e084f3d66320b5c1e37a65',1,'PyPR2']]],
+  ['registerrawtrajectoryinput',['registerRawTrajectoryInput',['../classPyPR2.html#ada2ccc68982ac78c5f36dfc9662767fc',1,'PyPR2']]],
+  ['registertiltscancallback',['registerTiltScanCallback',['../classPyPR2.html#a885819cd4436d03ad8990b982a6a8c6f',1,'PyPR2']]]
 ];
