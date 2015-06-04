e44d52c9ee2478682704d73f2d99794269a4ef69
Xun Wang <wang.xun@gmail.com>
2015-06-05 01:33:11 +1000

    Update doc.

--- search/all_75.js
+++ search/all_75.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['usejointvelocitycontrol',['useJointVelocityControl',['../classPyPR2.html#abf328f324a602d6b43a6018a503d7368',1,'PyPR2']]]
+  ['usejointvelocitycontrol',['useJointVelocityControl',['../classPyPR2.html#abf328f324a602d6b43a6018a503d7368',1,'PyPR2']]],
+  ['usemoveit',['useMoveIt',['../classPyPR2.html#ab06a2dce82b816b110884bd4dd88117a',1,'PyPR2']]]
 ];
--- search/functions_6d.js
+++ search/functions_6d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['movearmposeto',['moveArmPoseTo',['../classPyPR2.html#a635bf37c0c33ae177acef3e6836840d5',1,'PyPR2']]],
+  ['movearmto',['moveArmTo',['../classPyPR2.html#a12079d0eac454eeda76fdc62959d14e2',1,'PyPR2']]],
   ['movearmwithjointpos',['moveArmWithJointPos',['../classPyPR2.html#a2d8ecb78d985e019a87ee85ac1d8710b',1,'PyPR2']]],
   ['movearmwithjointtrajectory',['moveArmWithJointTrajectory',['../classPyPR2.html#add75b3895e90c446186a4744d624d950',1,'PyPR2']]],
   ['movearmwithjointtrajectoryandspeed',['moveArmWithJointTrajectoryAndSpeed',['../classPyPR2.html#a738d5bd8b08d825c3d2a3a8d7bee1126',1,'PyPR2']]],
