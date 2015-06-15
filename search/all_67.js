3d337890e5c3cb0d470e471ecaf7a7916ddab11f
Xun Wang <wang.xun@gmail.com>
2015-06-15 18:41:46 +1000

    doc refresh.

--- search/all_67.js
+++ search/all_67.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['getarmjointpositions',['getArmJointPositions',['../classPyPR2.html#ad34ee9430b52db397f40faaeb1614c8a',1,'PyPR2']]],
+  ['getarmpose',['getArmPose',['../classPyPR2.html#a0e091e88e66473f4ded76513cdb8d2d7',1,'PyPR2']]],
   ['getbatterystatus',['getBatteryStatus',['../classPyPR2.html#acdfa8d305b9a6eb75886110fbe4140d1',1,'PyPR2']]],
   ['getjointpos',['getJointPos',['../classPyPR2.html#a8044baa2fb267fc4daa797ab791c2b97',1,'PyPR2']]],
   ['getlowpowerthreshold',['getLowPowerThreshold',['../classPyPR2.html#a393e3fa09ed5b08ea95c614650135627',1,'PyPR2']]],
--- search/functions_72.js
+++ search/functions_72.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['recallrhythdmptrajectory',['recallRhythDMPTrajectory',['../classPyPR2.html#a2363d4e5074559cf635a866921ad8360',1,'PyPR2']]],
+  ['recallrhythdmptrajectory',['recallRhythDMPTrajectory',['../classPyPR2.html#a23a621f40e9ca7c82d4e04f20befd1ee',1,'PyPR2']]],
   ['registerbasescancallback',['registerBaseScanCallback',['../classPyPR2.html#a73c49857323bd14f75c3301eca159b6e',1,'PyPR2']]],
   ['registerhumandetecttracking',['registerHumanDetectTracking',['../classPyPR2.html#a3f510a5817e084f3d66320b5c1e37a65',1,'PyPR2']]],
   ['registerrawtrajectoryinput',['registerRawTrajectoryInput',['../classPyPR2.html#ada2ccc68982ac78c5f36dfc9662767fc',1,'PyPR2']]],
